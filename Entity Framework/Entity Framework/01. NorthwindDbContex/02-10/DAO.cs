2cfde697ff86fefcc54bb66f173b74313748ba6e
RosenTodorov <kobe_8@abv.bg>
2014-11-12 19:40:34 +0200

    Database & SQL - Tasks
    Database & SQL - Tasks

---
+++ Entity Framework/Entity Framework/01. NorthwindDbContex/02-10/DAO.cs
@@ -0,0 +1,296 @@
+﻿namespace _01.NorthwindDbContex
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using _01.NorthwindDbContex;
+
+    public static class DAO
+    {
+        public static void FindSalesByRegionAndPeriod(string region, string startDate = null, string endDate = null)
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                DateTime startDateDt = Convert.ToDateTime(startDate);
+                DateTime endDateDt = Convert.ToDateTime(endDate);
+
+                var salesByRegionAndPeriod = northwindDbContex
+                                            .Orders
+                                            .Where(o => o.ShipRegion == region &&
+                                                    o.OrderDate >= startDateDt && o.OrderDate <= endDateDt)
+                                            .GroupBy(o => o.ShipName);
+
+                foreach (var item in salesByRegionAndPeriod)
+                {
+                    Console.WriteLine(item.Key);
+                }
+
+            }
+        }
+
+        public static void InsertCustomer(string customerId, string companyName, string contactName = null,
+            string contactTitle = null, string address = null, string city = null, string region = null,
+            string postalCode = null, string country = null, string phone = null, string fax = null)
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                Customer newCustomer = new Customer
+                {
+                    CustomerID = customerId,
+                    CompanyName = companyName,
+                    ContactName = contactName,
+                    ContactTitle = contactTitle,
+                    Address = address,
+                    City = city,
+                    Region = region,
+                    PostalCode = postalCode,
+                    Country = country,
+                    Phone = phone,
+                    Fax = fax
+                };
+
+                northwindDbContex.Customers.Add(newCustomer);
+                northwindDbContex.SaveChanges();
+                Console.WriteLine("1 row affected.");
+            }
+
+        }
+
+        public static void UpdateCustomer(string customerId, string companyName, string contactName = null,
+            string contactTitle = null, string address = null, string city = null, string region = null,
+            string postalCode = null, string country = null, string phone = null, string fax = null)
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                Customer customer = northwindDbContex.Customers.First(c => c.CustomerID == customerId);
+
+                customer.CompanyName = companyName ?? customer.CompanyName;
+                customer.ContactName = contactName ?? customer.ContactName;
+                customer.ContactTitle = contactTitle ?? customer.ContactTitle;
+                customer.Address = address ?? customer.Address;
+                customer.City = city ?? customer.City;
+                customer.Region = region ?? customer.Region;
+                customer.PostalCode = postalCode ?? customer.PostalCode;
+                customer.Country = country ?? customer.Country;
+                customer.Phone = phone ?? customer.Phone;
+                customer.Fax = fax ?? customer.Fax;
+
+                northwindDbContex.SaveChanges();
+                Console.WriteLine("! row affected.");
+            }
+        }
+
+        public static void DeleteCustomer(string customerId)
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                Customer customer = northwindDbContex.Customers.First(c => c.CustomerID == customerId);
+
+                northwindDbContex.Customers.Remove(customer);
+                northwindDbContex.SaveChanges();
+
+                Console.WriteLine("1 row removed.");
+            }
+        }
+
+        public static void FindCustomersWithOrdersFrom1997ToCanade()
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                var customers = northwindDbContex.Orders
+                                                    .Where(o => o.OrderDate.Value.Year == 1997 &&
+                                                        o.ShipCountry == "Canada")
+                                                    .GroupBy(o => o.Customer.CompanyName)
+                                                    .ToList();
+
+                northwindDbContex.SaveChanges();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer.Key);
+                }
+            }
+        }
+
+        public static void FindCustomersWithOrdersFrom1997ToCanadeNativeSQL()
+        {
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            string querySql = "SELECT c.CompanyName FROM Orders o " +
+                              "JOIN Customers c ON c.CustomerID  = o.CustomerID " +
+                              "WHERE YEAR(o.OrderDate) = 1997 And o.ShipCountry = 'Canada' " +
+                              "GROUP BY c.CompanyName";
+
+            var customers = northwindDbContex.Database.SqlQuery<string>(querySql);
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+    }
+}
+/*  public static void InsertCustomer(string customerID, string companyName, string contactName, string contactTitle, string address, string city,
+              string region, string postalCode, string country, string phone, string fax)
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  bool success = false;
+                  try
+                  {
+                      Customer newCustomer = new Customer
+                      {
+                          CustomerID = customerID,
+                          CompanyName = companyName,
+                          ContactName = contactName,
+                          ContactTitle = contactTitle,
+                          Address = address,
+                          City = city,
+                          Region = region,
+                          PostalCode = postalCode,
+                          Country = country,
+                          Phone = phone,
+                          Fax = fax
+                      };
+
+                      if (baseConn.Customers.Find(newCustomer.CustomerID) == null && newCustomer.CustomerID != null)
+                      {
+                          baseConn.Customers.Add(newCustomer);
+                          baseConn.SaveChanges();
+                          success = true;
+                      }
+                  }
+                  finally
+                  {
+                      if (success)
+                      {
+                          Console.WriteLine("Customer added");
+                      }
+                      else
+                      {
+                          Console.WriteLine("Custumer not added");
+                      }
+                  }
+              }
+          }
+          public static void DeleteCustomer(string customerID)
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  bool success = false;
+
+                  try
+                  {
+                      var customerToRemove = baseConn.Customers.Find(customerID);
+                      baseConn.Customers.Remove(customerToRemove);
+                      baseConn.SaveChanges();
+                      success = true;
+                  }
+                  finally
+                  {
+                      if (success)
+                      {
+                          Console.WriteLine("Custumer deleted");
+                      }
+                      else
+                      {
+                          Console.WriteLine("Custumer could not be deleted");
+                      }
+                  }
+              }
+          }
+          public static void ModifyCustomer(string customerID, string companyName)
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  var customerToModify = baseConn.Customers.Find(customerID);
+                  customerToModify.CompanyName = companyName;
+                  baseConn.SaveChanges();
+              }
+          }
+
+          public static Customer GetCustomerByCriterion(string criterion, string searchValue)
+          {
+              Customer result = null;
+
+              using (var baseConn = new NorthwindEntities())
+              {
+                  foreach (var item in baseConn.Customers)
+                  {
+                      var itemProp = item.GetType().GetProperty(criterion);
+                      if (itemProp.GetValue(item, null).ToString() == searchValue)
+                      {
+                          result = item;
+                          break;
+                      }
+
+                  }
+              }
+
+              return result;
+          }
+
+          //03. Write a method that finds all customers who have orders made in 1997 and shipped to Canada.
+          public static void PrintOrders()
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  var customers = baseConn.Customers.Where(x => x.Orders.Where(o => o.OrderDate.Value.Year == 1997 && o.ShipCountry == "Canada").Any());
+
+                  foreach (var item in customers)
+                  {
+                      Console.WriteLine(item.CustomerID);
+                  }
+              }
+          }
+
+          //04. Implement previous by using native SQL query and executing it through the ObjectContext.
+          public static void PrintOrderWithSQL()
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  var customers = baseConn.Database.SqlQuery<string>(@"SELECT distinct c.CustomerID
+                                                                       FROM Customers c JOIN Orders o
+                                                                       ON c.CustomerID = o.CustomerID                
+                                                                       WHERE o.ShipCountry ='Canada' AND o.OrderDate like '%1997'");
+                  foreach (var item in customers)
+                  {
+                      Console.WriteLine(item);
+                  }
+              }
+          }
+
+          //05.Write a method that finds all the sales by specified region and period (start / end dates).
+          public static void PrintSalesByRegionAndPeriod()
+          {
+              var query = SelectOrdersByRegionAndDate("NM", new DateTime(1996, 01, 01), new DateTime(1997, 01, 01));
+              foreach (var res in query)
+              {
+                  Console.WriteLine(res);
+              }
+          }
+          private static ICollection<string> SelectOrdersByRegionAndDate(string region, DateTime start, DateTime end)
+          {
+              using (var baseConn = new NorthwindEntities())
+              {
+                  var result = baseConn.Orders.Where(
+                      x => x.ShipRegion == region && x.OrderDate >= start && x.OrderDate <= end).Select(
+                      c => c.Customer.CompanyName + " " + c.ShipAddress + " " + c.ShipCountry).ToList();
+
+                  return result;
+              }
+          }
+      }
+  } */
---
+++ Entity Framework/Entity Framework/01. NorthwindDbContex/02-10/TestDAO.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.NorthwindDbContex
+{
+    using System;
+
+    public class TestDAO
+    {
+     public static void Main()
+        {
+            //02. Create a DAO class with static methods which provide functionality for inserting, modifying and deleting customers. Write a testing class.
+
+            DAO.InsertCustomer("PESHO", "Telerik Academy");
+            DAO.UpdateCustomer("PESHO", "NESTLE");
+            DAO.DeleteCustomer("PESHO");
+
+            //03. Write a method that finds all customers who have orders made in 1997 and shipped to Canada.
+
+            DAO.FindCustomersWithOrdersFrom1997ToCanade();
+
+            //04. Implement previous by using native SQL query and executing it through the DbContext.
+
+            DAO.FindCustomersWithOrdersFrom1997ToCanadeNativeSQL();
+
+            //05. Write a method that finds all the sales by specified region and period (start / end dates).
+
+            DAO.FindSalesByRegionAndPeriod("RJ", "1995-01-01", "1999-09-12");
+
+            //06. Create a database called NorthwindTwin with the same structure as Northwind using the features from DbContext. Find for the API for schema generation in MSDN or in Google.
+
+            //Just changing the database name in the app.congig file makes EF to create it if it does not exist by the model.
+            NorthwindEntities northwindTwinDbContex = new NorthwindEntities();
+            northwindTwinDbContex.Database.CreateIfNotExists();
+
+            //07. Try to open two different data contexts and perform concurrent changes on the same records. What will happen at SaveChanges()? How to deal with it?
+         /*
+            NorthwindEntities secondNorthwindTwinDbContex = new NorthwindEntities();
+
+            using (northwindTwinDbContex)
+            {
+                using (secondNorthwindTwinDbContex)
+                {
+                    DAO.InsertCustomer("MMM", "Microsoft");
+                    DAO.UpdateCustomer("MMM", "Telerik");
+            
+                    Customer customer = northwindTwinDbContex.Customers.Find("MMM");
+                    customer.ContactName = "Gosho";
+            
+                    Customer sameCustomer = secondNorthwindTwinDbContex.Customers.Find("MMM");
+                    sameCustomer.ContactName = "Misho";
+            
+                    northwindTwinDbContex.SaveChanges();
+                    secondNorthwindTwinDbContex.SaveChanges();
+                }
+            } */
+          
+            //08. By inheriting the Employee entity class create a class which allows employees to access their corresponding territories as property of type EntitySet<T>.
+
+            NorthwindEntities northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                Employee employee = northwindDbContex.Employees.First(); 
+
+                foreach (var territory in employee.Territories)
+                {
+                    Console.WriteLine(territory.TerritoryDescription);
+                }
+            }
+
+            //09. Create a method that places a new order in the Northwind database. The order should contain several order items. Use transaction to ensure the data consistency.
+
+            northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                using (var dbContextTransaction = northwindDbContex.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        northwindDbContex.Orders.Add(
+                                    new Order
+                                    {
+                                        CustomerID = "ROMEY",
+                                        EmployeeID = 4
+                                    });
+                        northwindDbContex.SaveChanges();
+
+                        Order order = northwindDbContex
+                                        .Orders
+                                        .Where(o => o.CustomerID == "ROMEY" && o.EmployeeID == 4)
+                                        .FirstOrDefault();
+
+                        Console.WriteLine(order.CustomerID);
+                        dbContextTransaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        dbContextTransaction.Rollback();
+                    }
+                }
+            }
+
+          //10. Create a stored procedures in the Northwind database for finding the total incomes for given supplier name and period (start date, end date). Implement a C# method that calls the stored procedure and returns the retuned record set.
+
+            northwindDbContex = new NorthwindEntities();
+
+            using (northwindDbContex)
+            {
+                northwindDbContex.Database.ExecuteSqlCommand("CREATE PROCEDURE usp_GetIncomeByGivenCompany " +
+                "@companyName nvarchar(60), @startDate date, @endDate date AS " +
+                "SELECT s.CompanyName, SUM(od.UnitPrice * od.Quantity) AS Income " +
+                "FROM Suppliers s " +
+                "JOIN Products p " +
+                "ON s.SupplierID = p.SupplierID " +
+                "JOIN [Order Details] od " +
+                "ON p.ProductID = od.ProductID " +
+                "JOIN Orders o " +
+                "ON od.OrderID = o.OrderID " +
+                "where o.ShippedDate > @startDate AND o.ShippedDate < @endDate " +
+                "AND s.CompanyName = @companyName " +
+                "GROUP BY s.CompanyName");
+
+                string companyName = "Exotic Liquids";
+                DateTime startDate = new DateTime(1990, 1, 1);
+                DateTime endDate = new DateTime(1999, 1, 1);
+
+                var col = northwindDbContex.usp_GetIncomeByGivenCompany(companyName, startDate, endDate).First();
+                Console.WriteLine("Company name: {0} -> Income: {1}", col.CompanyName, col.Income);
+            }
+        }
+    }
+}
+     
